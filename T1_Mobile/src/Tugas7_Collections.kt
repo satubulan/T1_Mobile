253a3f6d762a426f2a559cab52c755899c79387e
F1D02310040_AYU DIANA <dshine377@gmail.com>
2026-03-10 13:29:25 +0800

    Update Tugas7_Collections.kt

--- T1_Mobile/src/Tugas7_Collections.kt
+++ T1_Mobile/src/Tugas7_Collections.kt
@@ -1,11 +1,11 @@
 fun main (){
-    data class NilaiMahasiswa(
+    data class NilaiMahasiswa( //KONSTRUKTOR SEBAGAI INISIALISASI DATA MAHASISWA
         val nim: String,
         val nama: String,
         val matakuliah: String,
         val nilai: Int
     )
-    fun getGrade(nilai: Int): String {
+    fun getGrade(nilai: Int): String { //FUNCTION GRADE NILAI
         return when {
             nilai >= 85 -> "A"
             nilai >= 75 -> "B"
@@ -14,7 +14,7 @@ fun main (){
             else        -> "E"
         }
     }
-    val dataMahasiswa = listOf(
+    val dataMahasiswa = listOf(//10 DATA MAHASISWA DENGAN VARIABEL SESUAI DENGAN CLASS DATA
         NilaiMahasiswa("F1D02310040", "Ayu Diana", "Pemrograman Bergerak", 100),
         NilaiMahasiswa("F1D0200001", "Eka", "Pemrograman Bergerak", 70),
         NilaiMahasiswa("F1D0200002",  "Budi Santoso", "Pemrograman Bergerak", 85),
@@ -27,7 +27,7 @@ fun main (){
         NilaiMahasiswa("F1D0200009",  "Joko Susilo", "Pemrograman Bergerak", 78)
     )
     println("${"No".padEnd(5)}${"NIM".padEnd(15)}${"Nama".padEnd(20)}${"MataKuliah".padEnd(25)}Nilai")
-    for ((index, mhs) in dataMahasiswa.withIndex()) {
+    for ((index, mhs) in dataMahasiswa.withIndex()) { //MENAMPILKAN LIST MAHASISWA
         val no   = (index + 1).toString().padEnd(5)
         val nim  = mhs.nim.padEnd(15)
         val nama = mhs.nama.padEnd(20)
@@ -35,29 +35,28 @@ fun main (){
         println("$no$nim$nama$mk${mhs.nilai}")
     }
     println("=====STATISTIK=====")
-    println("Total Mahasiswa : ${dataMahasiswa.count()}")
-    val totalNilai = dataMahasiswa.sumOf { it.nilai }
-    val rataRata = totalNilai / dataMahasiswa.count()
+    println("Total Mahasiswa : ${dataMahasiswa.count()}") // MENGHITUNG JUMLAH MAHASISWA
+    val totalNilai = dataMahasiswa.sumOf { it.nilai } // MENJUMLAHKAN NILAI SEMUA MAHASISWA
+    val rataRata = totalNilai / dataMahasiswa.count() // MEMPEROLEH RATA RATA NILAI MAHASISWA BERDASARKAN TOTAL NILAI KESELURUHAN : JUMLAH MAHASISWA
     println("Rata-rata Nilai : $rataRata")
-    val mahasiswaBest = dataMahasiswa.maxBy { it.nilai }
+    val mahasiswaBest = dataMahasiswa.maxBy { it.nilai } // MENCARI MAHASISWA DENGAN NILAI TERTINGGI
     println("Nilai Tertinggi : ${mahasiswaBest.nilai} (${mahasiswaBest.nama})")
-    val mahasiswaMin = dataMahasiswa.minBy { it.nilai }
+    val mahasiswaMin = dataMahasiswa.minBy { it.nilai } // MENCARI MAHASISWA DENGAN NILAI TERENDAH
     println("Nilai Terendah : ${mahasiswaMin.nilai} (${mahasiswaMin.nama})")
 
     println("=====MAHASISWA LULUS=====")
-    for ((index, mhs) in dataMahasiswa.withIndex()) {
+    for ((index, mhs) in dataMahasiswa.withIndex()) { //MENAMPILKAN MAHASISWA LULUS DARI INDEX DENGAN MEMVALIDASI NILAI MAHASISWA HARUS LEBIH BESAR 70 SEDANGKAN YANG LEBIH KECIL DARI 70 TIDAK DI TAMPILKAN
         if (mhs.nilai<70) continue
         println("${(index + 1).toString().padEnd(5)}${mhs.nama.padEnd(20)}${mhs.nilai}")
-
     }
     println("=====MAHASISWA TIDAK LULUS=====")
-    for ((index, mhs) in dataMahasiswa.withIndex()) {
+    for ((index, mhs) in dataMahasiswa.withIndex()) {  //MENAMPILKAN MAHASISWA TIDAK LULUS DARI INDEX DENGAN MEMVALIDASI NILAI MAHASISWA HARUS LEBIH KECIL DARI 70 SEDANGKAN YANG LEBIH BESAR DARI 70 TIDAK DI TAMPILKAN
         if (mhs.nilai>=70) continue
         println("${(index + 1).toString().padEnd(5)}${mhs.nama.padEnd(20)}${mhs.nilai}")
 
     }
     println("\n=====URUTAN NILAI ASCENDING=====")
-    val ascending = dataMahasiswa.sortedBy { it.nilai }
+    val ascending = dataMahasiswa.sortedBy { it.nilai } MENGURUTKAN MAHASISWA SECARA ASCENDING
     for ((index, mhs) in ascending.withIndex()) {
         println("${(index+1).toString().padEnd(5)}${mhs.nama.padEnd(20)}${mhs.nilai}")
     }
